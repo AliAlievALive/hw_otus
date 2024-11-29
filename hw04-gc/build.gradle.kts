779c3ee03c9f4900149b26fa3d1fad5bb8f8b0aa
Almir.Almiev <Almir.Almiev@novaenergies.net>
2024-11-29 22:56:18 +0300

    Added HW

---
+++ hw04-gc/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("java")
+}
+
+group = "org.otus"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("ch.qos.logback:logback-classic")
+}
