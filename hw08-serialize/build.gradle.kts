9156ab54cfe3eb3dacfeccdf17957e1a6df332bb
Almir.Almiev <Almir.Almiev@novaenergies.net>
2025-01-06 17:35:37 +0300

    Added HW serialize

---
+++ hw08-serialize/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "org.otus"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("ch.qos.logback:logback-classic")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+
+    testImplementation ("org.junit.jupiter:junit-jupiter-api")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation ("org.assertj:assertj-core")
+    testImplementation ("org.mockito:mockito-junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
