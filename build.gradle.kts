249e8fe5e5215ef264cc0832e4bf01b52caf32a5
almir.almiev <almir.almiev@thebigdipper.com>
2025-02-01 14:22:37 +0300

    fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,8 @@ plugins {
     idea
     id("io.spring.dependency-management")
     id("org.springframework.boot") apply false
-    id("java")
+    id("name.remal.sonarlint") apply false
+    id("com.diffplug.spotless") apply false
 }
 
 idea {
@@ -27,15 +28,46 @@ allprojects {
         mavenCentral()
     }
 
+    val testcontainersBom: String by project
+    val protobufBom: String by project
     val guava: String by project
+    val jmh: String by project
+    val asm: String by project
+    val glassfishJson: String by project
 
     apply(plugin = "io.spring.dependency-management")
     dependencyManagement {
         dependencies {
             imports {
                 mavenBom(BOM_COORDINATES)
+                mavenBom("org.testcontainers:testcontainers-bom:$testcontainersBom")
+                mavenBom("com.google.protobuf:protobuf-bom:$protobufBom")
             }
             dependency("com.google.guava:guava:$guava")
+            dependency("org.openjdk.jmh:jmh-core:$jmh")
+            dependency("org.openjdk.jmh:jmh-generator-annprocess:$jmh")
+            dependency("org.glassfish:jakarta.json:$glassfishJson")
+            dependency("org.ow2.asm:asm-commons:$asm")
+        }
+    }
+
+    configurations.all {
+        resolutionStrategy {
+            failOnVersionConflict()
+
+            force("javax.servlet:servlet-api:2.5")
+            force("commons-logging:commons-logging:1.1.1")
+            force("commons-lang:commons-lang:2.5")
+            force("org.codehaus.jackson:jackson-core-asl:1.8.8")
+            force("org.codehaus.jackson:jackson-mapper-asl:1.8.8")
+            force("org.sonarsource.analyzer-commons:sonar-analyzer-commons:2.8.0.2699")
+            force("org.sonarsource.analyzer-commons:sonar-xml-parsing:2.8.0.2699")
+            force("org.sonarsource.sslr:sslr-core:1.24.0.633")
+            force("org.sonarsource.analyzer-commons:sonar-analyzer-recognizers:2.8.0.2699")
+            force("com.google.code.findbugs:jsr305:3.0.2")
+            force("commons-io:commons-io:2.15.1")
+            force("com.google.errorprone:error_prone_annotations:2.26.1")
+            force("com.google.j2objc:j2objc-annotations:3.0.0")
         }
     }
 }
@@ -50,6 +82,17 @@ subprojects {
     tasks.withType<JavaCompile> {
         options.encoding = "UTF-8"
         options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing"))
+
+        dependsOn("spotlessApply")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging.showExceptions = true
+        reports {
+            junitXml.required.set(true)
+            html.required.set(true)
+        }
     }
 }
 
