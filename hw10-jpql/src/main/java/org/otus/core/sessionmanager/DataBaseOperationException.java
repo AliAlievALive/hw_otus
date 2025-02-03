92f50c36c6a51c47d6a18bee8f7885a2618f15ef
almir.almiev <almir.almiev@thebigdipper.com>
2025-02-03 13:17:51 +0300

    deleted hw 10

--- hw10-jpql/src/main/java/org/otus/core/sessionmanager/DataBaseOperationException.java
+++
@@ -1,7 +0,0 @@
-package org.otus.core.sessionmanager;
-
-public class DataBaseOperationException extends RuntimeException {
-    public DataBaseOperationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
